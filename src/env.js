2aee8f3991fd942fe7ab592e854a8b675bd78e89
Frederick Adebayo <sprintcorp7@gmail.com>
2021-11-28 08:41:21 +0100

    update

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://127.0.0.1:5000/';
-// export const BASE_URL = 'https://twiitee.herokuapp.com/';
+// export const BASE_URL = 'http://127.0.0.1:5000/';
+export const BASE_URL = 'https://twiitee.herokuapp.com/';
 export const AUTH_TOKEN = 'access_token';
 export const NAME = 'name';
 export const ID = 'id';
